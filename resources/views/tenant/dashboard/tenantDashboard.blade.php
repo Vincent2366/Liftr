1e576c2756e0485ffaadd6412cc1bb418d327257
Vincent <2001105940@student.buksu.edu.ph>
2025-04-27 20:57:17 +0800

    added pa chuy chuy aron chuy

--- resources/views/tenant/dashboard/tenantDashboard.blade.php
+++ resources/views/tenant/dashboard/tenantDashboard.blade.php
@@ -182,18 +182,18 @@
 
 
                        
-                        <!-- Clients Card -->
+                        <!-- Active Users Card -->
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-warning shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Sessions</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $clientCount ?? '0' }}</div>
+                                                Active Users</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\Activity::where('status', 'active')->whereNull('end_time')->count() }}</div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-user-tie fa-2x text-gray-300"></i>
+                                            <i class="fas fa-running fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -356,6 +356,7 @@
 </body>
 
 </html>
+
 
 
 
