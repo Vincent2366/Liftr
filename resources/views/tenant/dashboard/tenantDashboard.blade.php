26a285389009ead023683f24565ae118487eb29d
Vincent <2001105940@student.buksu.edu.ph>
2025-04-27 21:36:39 +0800

    sdasd

--- resources/views/tenant/dashboard/tenantDashboard.blade.php
+++ resources/views/tenant/dashboard/tenantDashboard.blade.php
@@ -168,20 +168,21 @@
                        
                         <!-- Active Users Card -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Active Users</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\Activity::where('status', 'active')->whereNull('end_time')->count() }}</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-running fa-2x text-gray-300"></i>
+                            <a href="{{ route('tenant.sessions') }}" class="text-decoration-none">
+                                <div class="card border-left-warning shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                    Active Users</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\Activity::where('status', 'active')->whereNull('end_time')->count() }}</div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-running fa-2x text-gray-300"></i>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
                         </div>
                     </div>
 
